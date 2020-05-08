18509d1d0dd1b8bacc508f69311f84a1f7bafdab
Ashwin Prasanth <62993493+ashwinpra@users.noreply.github.com>
2020-05-08 13:29:49 +0400

    .

--- CollatzConjecture.py
+++ CollatzConjecture.py
@@ -11,16 +11,18 @@ def reachesOne(num):
     else:
         return False
 
+print("The Collatz conjecture is a conjecture in mathematics that concerns a sequence defined as follows:\nstart with any positive integer n. Then each term is obtained from the previous term as follows:\nif the previous term is even, the next term is one half of the previous term.\nIf the previous term is odd, the next term is 3 times the previous term plus 1.\nThe conjecture is that no matter what value of n, the sequence will always reach 1.")
+print("------------------------------------------------------------------------------------------------\n")
 
 count=0
 num=[]
-test=int(input("Upper limit for test?"))
+test=int(input("How many numbers should be tested?\n"))
 for i in range(1,test):
     if  not reachesOne(i):
         count+=1
         num.append(i)
 if count>0:
     print("The Collatz Conjecture is disproven!")
-    print(i+"do(es) not follow Collatz Conjecture")
+    print(i+"disprove(s) the Conjecture!")
 else:
-    print("Collatz is a genius")
+    print("Collatz Conjecture holds true!")
