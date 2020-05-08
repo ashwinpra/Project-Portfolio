de33098eeb5dc04f3d3055f8e6474958071a3585
ashwinpra <62993493+ashwinpra@users.noreply.github.com>
2020-05-08 12:58:18 +0400

    .

--- WhatsappSpamBot.py
+++ WhatsappSpamBot.py
@@ -1,15 +1,22 @@
-from pynput.keyboard import Key, Controller
-import time
+#another useless program that can be used to spam a whatsapap user with messages!
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+path = "/Users/User/Downloads/chromedriver"
+driver = webdriver.Chrome(path)
+driver.get("https://web.whatsapp.com/")
+driver.implicitly_wait(10)
+name = input('Enter the name of user or group : ')
+msg = input('Enter your message : ')
+count = int(input('Enter the count : '))
 
-Keyboard=Controller()
-time.sleep(10)
+input('Enter anything after scanning QR code')
 
-while True:
-    for i in "Happy Birthday Aidan":
+user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
+user.click()
 
-        Keyboard.press(i)
-        Keyboard.release(i)
-       
-    Keyboard.press(Key.enter)
-    Keyboard.release(Key.enter)
+msg_box = driver.find_element_by_class_name('_2S1VP')
 
+for i in range(count):
+    msg_box.send_keys(msg)
+    button = driver.find_element_by_class_name('_35EW6')
+    button.click()
